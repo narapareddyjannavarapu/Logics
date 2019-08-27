6a543820f80655874aa39bb5f5298d8c66f5ec18
ravibabu <raviqa437@gmail.com>
2019-08-27 13:09:39 +0530

    Version 1

---
+++ src/logical/convertchartoarray.java
@@ -0,0 +1,14 @@
+package logical;
+
+public class convertchartoarray 
+{
+
+	public static void main(String[] args) 
+	{
+		char[] ch = {'g', 'o', 'o', 'd', ' ', 'm', 'o', 'r', 'n', 'i', 'n', 'g'};
+	      String str = new String(ch);
+	      System.out.println(str);	
+     
+	}
+
+}
---
+++ src/logical/even.java
@@ -0,0 +1,17 @@
+package logical;
+
+public class even {
+
+	public static void main(String[] args) {
+		int n = 100;
+		
+		for (int i = 1; i <= n; i++) {
+		   
+		   if (i % 2 != 0) {
+			System.out.print(i + " ");
+		   }
+		}
+
+	}
+
+}
